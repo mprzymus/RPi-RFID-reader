4c835ec9f2c45c8c882e24471e3d44c97a9230d0
mprzymus <pzrymus@gmail.com>
2020-04-08 22:53:18 +0200

    Mqtt clients made. Need to implements all functionalities

--- MqttSender.py
+++ MqttSender.py
@@ -17,7 +17,7 @@ class MqttSender:
         self.client.publish("card_used", message)
 
 
-def generate_message(self, info_list):
+def generate_message(info_list):
     message = ""
     for element in info_list:
         message = message + "." + element
--- inputSimulator.py
+++ inputSimulator.py
@@ -1,4 +1,4 @@
-from Server import Server
+import MqttSender
 
 
 def get_key():
@@ -6,27 +6,31 @@ def get_key():
 
 
 def wait_for_input():
-    server = Server()
+    client = MqttSender.MqttSender("T1")
     while True:
         try:
             command = int(input("1 - card use, 2 - generate rapport, 3 - remove user, 4 - add card user, 5 - exit "))
             if command == 1:
                 key = get_key()
-                server.notify_card_usage(key)
+                client.send_message(MqttSender.generate_message([key]))
             elif command == 2:
                 key = get_key()
-                server.generate_rapport(key)
+                client.generate_rapport(key)
             elif command == 3:
                 key = get_key()
-                server.remove_user(key)
+                client.remove_user(key)
             elif command == 4:
                 key = get_key()
                 name = input("type user name ")
-                server.add_user(key, name)
+                client.add_user(key, name)
             elif command == 5:
                 break
             else:
                 print("wrong input %s" % command)
         except ValueError:
             print("Incorrect input")
-    del server
+    del client
+
+
+if __name__ == '__main__':
+    wait_for_input()
